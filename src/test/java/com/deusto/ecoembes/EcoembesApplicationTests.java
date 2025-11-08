80863dd3383ecece7eed866fbcebcceae09f59ed
Jon Ander Areizaga <jonander.areizaga@opendeusto.es>
2025-11-08 19:29:12 +0100

    Creación del proyecto

---
+++ src/test/java/com/deusto/ecoembes/EcoembesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deusto.ecoembes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoembesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
