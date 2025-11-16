1a87f7d2ac80d850b6bb86da451513d42e94d000
Jon Ander Areizaga <jonander.areizaga@opendeusto.es>
2025-11-16 20:22:03 +0100

    cambios en el scaneo de paquetes de EcoembesApplication

--- src/main/java/com/deusto/ecoembes/EcoembesApplication.java
+++ src/main/java/com/deusto/ecoembes/EcoembesApplication.java
@@ -1,14 +1,26 @@
 package com.deusto.ecoembes;
 
-import org.springframework.boot.SpringApplication;	
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.deusto.ecoembes", "Ecoembes"})
+@SpringBootApplication(
+    // Ya no necesitamos 'exclude' porque quitamos 'data-jpa' del gradle
+    
+    // Escaneamos TODOS los paquetes de tu proyecto
+    scanBasePackages = {
+        "com.deusto.ecoembes", // El paquete principal
+        "config",              // Los otros paquetes
+        "repository",
+        "entity",
+        "dto",
+        "service",
+        "facade"
+    }
+)
+
 public class EcoembesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcoembesApplication.class, args);
 	}
-
 }
-
