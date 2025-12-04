c70bcd82f03cb1670396652e899283e3232e3365
Jon Ander Areizaga <jonander.areizaga@opendeusto.es>
2025-12-04 11:09:11 +0100

    Prototipo 2 completo

--- src/main/java/Ecoembes/EcoembesApplication.java
+++ src/main/java/Ecoembes/EcoembesApplication.java
@@ -21,9 +21,9 @@ public class EcoembesApplication {
 	
 	public static void main(String[] args) {
 		System.out.println("═══════════════════════════════════════════════════════════");
-		System.out.println("   					SERVIDOR ECOEMBES");
-		System.out.println("   		Sistema de Gestión de Reciclaje de Envases");
-		System.out.println("   			Equipo DS-03 - Universidad de Deusto");
+		System.out.println("   		  SERVIDOR ECOEMBES");
+		System.out.println("Sistema de Gestión de Reciclaje de Envases");
+		System.out.println("  Equipo DS-03 - Universidad de Deusto");
 		System.out.println("═══════════════════════════════════════════════════════════");
 		
 		SpringApplication.run(EcoembesApplication.class, args);
